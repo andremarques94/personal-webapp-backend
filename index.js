26845d38a2a1d20ec6a432395ca5f896bc2bb31d
NOZK <andre.marques@academiadecodigo.org>
2024-05-02 15:51:40 +0100

    Add cors

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import emailRouter from "./src/routes/email-router.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/email", emailRouter);
+app.use("/", emailRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
