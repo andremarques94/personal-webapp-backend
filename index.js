ad6d83bcbba8f72d45563a89a0a5acbbdc6bfa2a
NOZK <andre.marques@academiadecodigo.org>
2024-05-02 16:20:35 +0100

    Change cors origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
